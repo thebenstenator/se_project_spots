b70f35dba740f94e20976705c43f8f0710c008c5
thebenstenator <thebenstenator@gmail.com>
2025-11-29 13:18:50 -0700

    fix post css config for compatibility

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-import autoprefixer from "autoprefixer";
-import cssnano from "cssnano";
-
 export default {
-  plugins: [autoprefixer, cssnano({ preset: "default" })],
+  plugins: {
+    autoprefixer: {},
+    cssnano: { preset: "default" },
+  },
 };
